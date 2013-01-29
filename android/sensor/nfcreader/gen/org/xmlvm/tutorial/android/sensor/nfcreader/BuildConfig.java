6548bf3e91c8dd21e9c20bd3f25e2d0bbb48b5ea
Arno Puder <arno@puder.org>
2013-01-28 16:51:38 -0800

    Add NfcTagReader

---
+++ android/sensor/nfcreader/gen/org/xmlvm/tutorial/android/sensor/nfcreader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.xmlvm.tutorial.android.sensor.nfcreader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
