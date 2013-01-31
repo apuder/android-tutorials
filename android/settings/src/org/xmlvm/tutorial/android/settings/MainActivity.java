d988b4dcfe294e91993b672e61e4393c5609301a
Arno Puder <arno@puder.org>
2013-01-30 22:12:16 -0800

    Doc for Settings

--- android/settings/src/org/xmlvm/tutorial/android/settings/MainActivity.java
+++ android/settings/src/org/xmlvm/tutorial/android/settings/MainActivity.java
@@ -8,6 +8,15 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * Settings are usually used to allow the user change the behavior of an app. In
+ * Android, the settings dialog is a dedicated activity, whose UI is defined by
+ * a special XML file that has to reside in the folder res/xml. In this example,
+ * the description of the settings is stored in file res/xml/settings.xml. An
+ * application-specific activity called SettingsActivity acts as a wrapper for
+ * the settings activity. The settings can be retrieved via the
+ * PreferenceManager class.
+ */
 public class MainActivity extends Activity {
 
     private TextView lblFullName;
@@ -31,13 +40,24 @@ public class MainActivity extends Activity {
     }
 
     public void openSettings(View v) {
+        /*
+         * Opening the settings dialog is simply a matter of starting the
+         * appropriate activity (SettingsActivity in this case).
+         */
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
     private void displaySettings() {
+        /*
+         * Individual settings can be retrieved via class PreferenceManager.
+         */
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
 
+        /*
+         * A specific setting is referenced via a key ('full_name') that has to
+         * match the definition in res/xml/settings.xml.
+         */
         String prefStr = pref.getString("full_name", "n.a.");
         lblFullName.setText(prefStr);
 
