6aaeb74e515be78004b89d5de4211c5bbac15175
Arno Puder <arno@puder.org>
2013-02-10 12:55:58 -0800

    Add documentation for bound service

--- android/service/bound/src/org/xmlvm/tutorial/android/service/bound/FibonacciResultListener.java
+++ android/service/bound/src/org/xmlvm/tutorial/android/service/bound/FibonacciResultListener.java
@@ -1,5 +1,10 @@
 package org.xmlvm.tutorial.android.service.bound;
 
+/**
+ * FibonacciResultListener is a simple interface that FibonacciService uses to
+ * return the results of a computation. FibonacciActivity implements this
+ * interface.
+ */
 public interface FibonacciResultListener {
 
     public void resultAvailable(final int n, final int res);
