7b2a278621be6ba8e9d9ae2705f8851c387c7b40
Arno Puder <arno@puder.org>
2013-02-10 13:33:58 -0800

    Add documentation for started service

--- android/service/started/src/org/xmlvm/tutorial/android/service/started/FibonacciActivity.java
+++ android/service/started/src/org/xmlvm/tutorial/android/service/started/FibonacciActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * FibonacciActivity is an example of a started service. Once the application is
+ * launched, type in an integer into the input field and press the button.
+ * FibonacciActivity will trigger an Intent that will start the service. The
+ * application will then deliberately exit (via Activity.finish()) to show what
+ * happens when the service finished the computation.
+ */
 public class FibonacciActivity extends Activity {
 
     private EditText edtInput;
@@ -20,6 +27,15 @@ public class FibonacciActivity extends Activity {
         edtInput = (EditText) findViewById(R.id.edt_fib);
         lblResult = (TextView) findViewById(R.id.lbl_result);
 
+        /*
+         * Retrieve the extra parameters that came with the Intent that was used
+         * to launch this activity. If the user launched this app from the
+         * homescreen, there won't be any additional parameters. If however the
+         * application was launched because the user tapped on the notification,
+         * the Intent will have additional parameters that denote the result of
+         * the service. In this case retrieve the results and show them on the
+         * UI.
+         */
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             int n = extras.getInt("n", -1);
@@ -34,9 +50,18 @@ public class FibonacciActivity extends Activity {
     public void compute(View v) {
         try {
             int n = Integer.parseInt(edtInput.getText().toString());
+            /*
+             * The service is started via an Intent that has the integer value
+             * as an extra parameter.
+             */
             Intent intent = new Intent(this, FibonacciService.class);
             intent.putExtra("n", n);
             startService(intent);
+            /*
+             * Explicitly exit the application. This is done to show the effect
+             * of the application not running when the service posts the results
+             * as a notification.
+             */
             finish();
         } catch (Exception exc) {
             lblResult.setText("Invalid input ...");
--- android/service/started/src/org/xmlvm/tutorial/android/service/started/FibonacciService.java
+++ android/service/started/src/org/xmlvm/tutorial/android/service/started/FibonacciService.java
@@ -7,30 +7,82 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * FibonacciService is an example of a started service. The service will
+ * automatically be started whenever a client sends an intent and will also
+ * automatically be terminated when the work item has been processed. Since the
+ * application might have terminated while the service was still processing the
+ * request, the results are posted as a notification. When the user taps on the
+ * notification the corresponding application (FibonacciActivity in this case)
+ * will automatically be launched.
+ * 
+ * FibonacciService is derived from IntentService which is a helper class to
+ * implement a simple message queuing mechanism for Intents that will
+ * automatically run in a background thread. IntentService is itself derived
+ * from class Service.
+ */
 public class FibonacciService extends IntentService {
 
     public FibonacciService() {
+        /*
+         * The string parameter "FibonacciService" is used as the name of the
+         * thread that will be created to process incoming intents.
+         */
         super("FibonacciService");
     }
 
+    /**
+     * onHandleIntent() will be called by the base class IntentService whenever
+     * there is an Intent to be processed. This method will be run in its own
+     * thread (automatically created by IntentService) so it can safely run long
+     * running operations. Intents are delivered one after another.
+     */
     @Override
     protected void onHandleIntent(Intent intent) {
+        /*
+         * Retrieve the parameter.
+         */
         int n = intent.getExtras().getInt("n");
+        /*
+         * Perform the actual computation.
+         */
         int res = fib(n);
+        /*
+         * Sleep for 5 seconds to mimic a long running operation.
+         */
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
         }
+        /*
+         * Show the results of the computation via a status bar notification.
+         */
         showNotification(n, res);
     }
 
+    /**
+     * showNotification() is a helper method that displays the result as a
+     * status bar notification. If the user taps on the notification, the
+     * corresponding application (FibonacciActivity) will automatically be
+     * launched.
+     */
     private void showNotification(int n, int res) {
+        /*
+         * Create a new notification with the help of the notification manager.
+         * Set a title, icon and sound for the notification and make it
+         * cancellable (i.e., the user can dismiss it from the status bar).
+         */
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = new Notification(R.drawable.ic_statuc_calc,
                 "Fibonacci result available", System.currentTimeMillis());
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
         notification.defaults |= Notification.DEFAULT_SOUND;
 
+        /*
+         * Create a notificationIntent that will be used to re-launch
+         * FibonacciActivity. Note that the intent contains the result of the
+         * computation.
+         */
         Context context = getApplicationContext();
         CharSequence contentTitle = "Fibonacci";
         CharSequence contentText = "New result available";
@@ -38,6 +90,12 @@ public class FibonacciService extends IntentService {
         notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         notificationIntent.putExtra("n", n);
         notificationIntent.putExtra("res", res);
+        /*
+         * The notificationIntent is wrapped by a contentIntent. The latter is
+         * used to create the notification that will be displayed in the status
+         * bar. When the user clicks on the notification, Android will send
+         * notificationIntent that in turn will re-launch the application.
+         */
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
                 PendingIntent.FLAG_CANCEL_CURRENT);
         notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
@@ -45,6 +103,9 @@ public class FibonacciService extends IntentService {
         notificationManager.notify(1, notification);
     }
 
+    /**
+     * Helper method to compute the Fibonacci value.
+     */
     private int fib(int n) {
         if (n == 0) {
             return 0;
