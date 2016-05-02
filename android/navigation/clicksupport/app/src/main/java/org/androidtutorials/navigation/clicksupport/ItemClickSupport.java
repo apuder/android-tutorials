2fb30ec7a35b99e5c2c2207311e1e64cbb3f6138
Pavitra Kansara <pmk.2429@gmail.com>
2016-05-02 16:33:33 -0700

    Add ItemClickSupport to provide onClick support to recyclerView without passing around onClick Listeners and decoupling the Adapter

---
+++ android/navigation/clicksupport/app/src/main/java/org/androidtutorials/navigation/clicksupport/ItemClickSupport.java
@@ -0,0 +1,126 @@
+package org.androidtutorials.navigation.clicksupport;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Custom Click Listener for RecyclerView or any other Template View that allows simple onclick listener callbacks without
+ * actually having to pass the onClick listeners.
+ */
+public class ItemClickSupport {
+    private final RecyclerView mRecyclerView;
+    private OnItemClickListener mOnItemClickListener;
+    private OnItemLongClickListener mOnItemLongClickListener;
+
+    /**
+     * Callback to handle onClick of a View or Layout on which ItemClickSupport is applied
+     */
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener != null) {
+                RecyclerView.ViewHolder holder = mRecyclerView.getChildViewHolder(v);
+                mOnItemClickListener.onItemClicked(mRecyclerView, holder.getAdapterPosition(), v);
+            }
+        }
+    };
+    /**
+     * Callback to handle onLongClick of a View or Layout on which ItemClickSupport is applied
+     */
+    private View.OnLongClickListener mOnLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            if (mOnItemLongClickListener != null) {
+                RecyclerView.ViewHolder holder = mRecyclerView.getChildViewHolder(v);
+                return mOnItemLongClickListener.onItemLongClicked(mRecyclerView, holder.getAdapterPosition(), v);
+            }
+            return false;
+        }
+    };
+
+    /**
+     * {@inheritDoc}
+     */
+    private RecyclerView.OnChildAttachStateChangeListener mAttachListener
+            = new RecyclerView.OnChildAttachStateChangeListener() {
+        @Override
+        public void onChildViewAttachedToWindow(View view) {
+            if (mOnItemClickListener != null) {
+                view.setOnClickListener(mOnClickListener);
+            }
+            if (mOnItemLongClickListener != null) {
+                view.setOnLongClickListener(mOnLongClickListener);
+            }
+        }
+
+        @Override
+        public void onChildViewDetachedFromWindow(View view) {
+
+        }
+    };
+
+    private ItemClickSupport(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+        mRecyclerView.setTag(R.id.item_click_support, this);
+        mRecyclerView.addOnChildAttachStateChangeListener(mAttachListener);
+    }
+
+    /**
+     * Static hook up for enabling onClicks Listener on RecyclerView
+     *
+     * @param view
+     * @return
+     */
+    public static ItemClickSupport addTo(RecyclerView view) {
+        ItemClickSupport support = (ItemClickSupport) view.getTag(R.id.item_click_support);
+        if (support == null) {
+            support = new ItemClickSupport(view);
+        }
+        return support;
+    }
+
+    /**
+     * Removes the listener from the associated RecyclerView
+     *
+     * @param view
+     * @return
+     */
+    public static ItemClickSupport removeFrom(RecyclerView view) {
+        ItemClickSupport support = (ItemClickSupport) view.getTag(R.id.item_click_support);
+        if (support != null) {
+            support.detach(view);
+        }
+        return support;
+    }
+
+    public ItemClickSupport setOnItemClickListener(OnItemClickListener listener) {
+        mOnItemClickListener = listener;
+        return this;
+    }
+
+    public ItemClickSupport setOnItemLongClickListener(OnItemLongClickListener listener) {
+        mOnItemLongClickListener = listener;
+        return this;
+    }
+
+    private void detach(RecyclerView view) {
+        view.removeOnChildAttachStateChangeListener(mAttachListener);
+        view.setTag(R.id.item_click_support, null);
+    }
+
+    /**
+     * Registers a Callback when the RecyclerView is clicked
+     */
+    public interface OnItemClickListener {
+
+        void onItemClicked(RecyclerView recyclerView, int position, View v);
+    }
+
+    /**
+     * Registers a callback when a RecyclerView has been long pressed
+     */
+    public interface OnItemLongClickListener {
+
+        boolean onItemLongClicked(RecyclerView recyclerView, int position, View v);
+    }
+}
---
+++ android/navigation/clicksupport/app/src/main/java/org/androidtutorials/navigation/clicksupport/ItemListAdapter.java
@@ -0,0 +1,67 @@
+package org.androidtutorials.navigation.clicksupport;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Class ItemAdapter manages the underlying data model and populates so-called ViewHolder instances
+ * <p>
+ * The adapter does not contain any OnClickListener interface(s) neither does the GestureDetector to recognize user inputs. This
+ * way the adapter stays clean and loosely coupled from the listeners which in turn adds more to faster performance while
+ * scrolling and also advocates clean as well as manageable code.
+ * </p>
+ */
+public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ListItemViewHolder> {
+
+    private static final String TAG = "~!@#$ItemListAdptr";
+    private List<String> itemList;
+
+    public ItemListAdapter(List<String> itemList) {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ListItemViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(ListItemViewHolder holder, int position) {
+        /*
+         * This method will be called whenever a ViewHolder should be populated
+         * for a particular item. The data that the ViewHolder will show will be
+         * retrieved from the underlying data model (list of strings in this
+         * case).
+         */
+        holder.itemNameView.setText(itemList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    /**
+     * ViewHolder pattern for better scrolling results
+     */
+    public class ListItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView itemNameView;
+
+        /**
+         * Inflates new ListItemViewHolder instance with the item layout
+         *
+         * @param itemView
+         */
+        public ListItemViewHolder(View itemView) {
+            super(itemView);
+            itemNameView = (TextView) itemView.findViewById(R.id.item_name);
+        }
+    }
+
+}
